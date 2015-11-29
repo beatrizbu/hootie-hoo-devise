edac1abe65c036f4c4f79a9519bc03071a35bb4f
Eric Litow <elitow@gmail.com>
2015-11-29 12:30:28 -0500

    Add migration User and Image to Users table

---
+++ db/migrate/20151129172534_add_userto_users.rb
@@ -0,0 +1,4 @@
+class AddUsertoUsers < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126012637) do
+ActiveRecord::Schema.define(version: 20151129172534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20151126012637) do
     t.string   "name"
     t.string   "provider"
     t.string   "uid"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
